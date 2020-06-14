ff0502aba4c95bb4746bc76b50a4fccf2ff0b174
Julio Albelda <juliet.xxl@gmail.com>
2020-06-14 17:23:47 +0200

    Ejercicio selectorFrutas mapState

--- vueCli/selectorfrutas/src/store/index.js
+++ vueCli/selectorfrutas/src/store/index.js
@@ -4,7 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    frutas: [ 
+      {nombre: 'Manzana', cantidad: '0'},
+      {nombre: 'Pera', cantidad: '0'},
+      {nombre: 'Platano', cantidad: '0'}
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
