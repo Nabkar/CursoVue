dda720ebb0a9650a68eec79bdffb843e7c36e0f2
Julio Albelda <juliet.xxl@gmail.com>
2020-07-12 12:27:47 +0200

    Registro y validaciones

--- vueCli/auth/src/store/index.js
+++ vueCli/auth/src/store/index.js
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {auth} from '../firebase'
+import router from "../router";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usuario: null,
+    error: null
   },
   mutations: {
+    setUsuario (state, payload) {
+      state.usuario = payload
+    },
+    setError (state, payload) {
+      state.error = payload
+    }
   },
   actions: {
+    crearUsuario ({commit}, usuario) {
+      auth.createUserWithEmailAndPassword(usuario.email, usuario.password)
+        .then (res => {
+          console.log(res)
+          const usuarioCreado = {
+            email: res.user.email,
+            uid: res.user.uid
+          }
+          commit('setUsuario', usuarioCreado)
+          router.push('/')
+        })
+        .catch (error => {
+          console.log(error)
+          commit('setError', error)
+        })
+    }
   },
   modules: {
   }
---
+++ vueCli/auth/src/views/Registro.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<h1>Registro de usuarios</h1>
+		<form @submit.prevent="crearUsuario({email: email, password: pass1})">
+			<input 
+				type="email"
+				placeholder="Ingresa tu correo"
+				v-model="email">
+			<input 
+				type="password"
+				placeholder="Password"
+				v-model="pass1">
+			<input 
+				type="password"
+				placeholder="Repite el password"
+				v-model="pass2">
+				<button type="submit" :disabled='!desactivar'>Registrar usuario</button>
+		</form>
+		<p>{{error}}</p>
+	</div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+	export default {
+		name: "Registro",
+		data () {
+			return {
+				email: '',
+				pass1: '',
+				pass2: ''
+			}
+		},
+		methods: {
+			...mapActions(['crearUsuario'])
+		},
+		computed: {
+			...mapState(['error']),
+			desactivar () {
+				return this.pass1 === this.pass2 && this.pass1.trim() !== '' && this.pass1.length > 5
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
