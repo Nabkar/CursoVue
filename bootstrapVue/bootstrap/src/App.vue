f66c3c0df3c4e4ec4a3dc1456d1ec0d09f9ab721
Julio Albelda <juliet.xxl@gmail.com>
2020-07-19 18:15:15 +0200

    Pruebas bootstrapVue

---
+++ bootstrapVue/bootstrap/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
+            <b-nav-item :to="{name: 'About'}" active-class="active">About</b-nav-item>
+            <b-nav-item :to="{name: 'Grid'}" active-class="active">Grid</b-nav-item>
+            <b-nav-item :to="{name: 'Cards'}" active-class="active">Cards</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-container>
+      <router-view />
+    </b-container>
+  </div>
+</template>
---
+++ bootstrapVue/bootstrap/src/views/Cards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-5">
+    <b-row class="mt-5">
+      <b-col cols="12" md="4">
+        <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio repudiandae provident culpa a nesciunt incidunt dicta praesentium error pariatur exercitationem voluptatem, quo necessitatibus veritatis eos, id fugit, adipisci sed quibusdam?</p>
+          <b-button variant="dark" href="#">Button</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio repudiandae provident culpa a nesciunt incidunt dicta praesentium error pariatur exercitationem voluptatem, quo necessitatibus veritatis eos, id fugit, adipisci sed quibusdam?</p>
+          <b-button variant="dark" href="#">Button</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio repudiandae provident culpa a nesciunt incidunt dicta praesentium error pariatur exercitationem voluptatem, quo necessitatibus veritatis eos, id fugit, adipisci sed quibusdam?</p>
+          <b-button variant="dark" href="#">Button</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
+
+<style>
+</style>
